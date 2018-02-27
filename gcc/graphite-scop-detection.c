ysat0/gcc

commit 3f07babbe80cf1f6b6c30e5b9cb8ffb500f5e642

re PR tree-optimization/84399 ([graphite] ICE in set_codegen_error, at graphite-isl-ast-to-gimple.c:206)
2018-02-16  Richard Biener  <rguenther@suse.de>

	PR tree-optimization/84399
	* graphite-scop-detection.c (scop_detection::stmt_simple_for_scop_p):
	For operands we can analyze at their definition make sure we can
	analyze them at each use as well.

	* gcc.dg/graphite/pr84399.c: New testcase.

From-SVN: r257723

gcc/graphite-scop-detection.c
@@ -1027,7 +1027,23 @@ scop_detection::stmt_simple_for_scop_p (sese_l scop, gimple *stmt,
 
     case GIMPLE_ASSIGN:
     case GIMPLE_CALL:
-      return true;
+      {
+	tree op;
+	ssa_op_iter i;
+	/* Verify that if we can analyze operands at their def site we
+	   also can represent them when analyzed at their uses.  */
+	FOR_EACH_SSA_TREE_OPERAND (op, stmt, i, SSA_OP_USE)
+	  if (scev_analyzable_p (op, scop)
+	      && !graphite_can_represent_expr (scop, bb->loop_father, op))
+	    {
+	      DEBUG_PRINT (dp << "[scop-detection-fail] "
+			   << "Graphite cannot represent stmt:\n";
+			   print_gimple_stmt (dump_file, stmt, 0,
+					      TDF_VOPS | TDF_MEMSYMS));
+	      return false;
+	    }
+	return true;
+      }
 
     default:
       /* These nodes cut a new scope.  */

gcc/testsuite/gcc.dg/graphite/pr84399.c
@@ -1,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -floop-nest-optimize -fno-tree-loop-im --param scev-max-expr-size=1" } */
+
+void
+h8 (int cv, int od)
+{
+  for (;;)
+    {
+      int ih = (__UINTPTR_TYPE__)&od;
+      if (cv == 0)
+	while (od < 1)
+	  {
+	    int lq;
+
+	    for (lq = 0; lq < 3; ++lq)
+	      for (ih = 0; ih < 4; ++ih)
+		od += lq;
+	  }
+      while (ih < 1)
+	{
+	}
+    }
+}

commit ad058810c450e15115a709f690e793b40d77377e

re PR libgomp/84466 (libgomp.graphite/force-parallel-8.c fails starting with r257723)
2018-02-27  Richard Biener  <rguenther@suse.de>

	PR tree-optimization/84466
	* graphite-scop-detection.c (scop_detection::stmt_simple_for_scop_p):
	Adjust last change to less strictly validate use operands.

From-SVN: r258035

gcc/graphite-scop-detection.c
@@ -1028,16 +1028,23 @@ scop_detection::stmt_simple_for_scop_p (sese_l scop, gimple *stmt,
     case GIMPLE_ASSIGN:
     case GIMPLE_CALL:
       {
-	tree op;
+	tree op, lhs = gimple_get_lhs (stmt);
 	ssa_op_iter i;
+	/* If we are not going to instantiate the stmt do not require
+	   its operands to be instantiatable at this point.  */
+	if (lhs
+	    && TREE_CODE (lhs) == SSA_NAME
+	    && scev_analyzable_p (lhs, scop))
+	  return true;
 	/* Verify that if we can analyze operands at their def site we
 	   also can represent them when analyzed at their uses.  */
 	FOR_EACH_SSA_TREE_OPERAND (op, stmt, i, SSA_OP_USE)
 	  if (scev_analyzable_p (op, scop)
-	      && !graphite_can_represent_expr (scop, bb->loop_father, op))
+	      && chrec_contains_undetermined
+		   (scalar_evolution_in_region (scop, bb->loop_father, op)))
 	    {
 	      DEBUG_PRINT (dp << "[scop-detection-fail] "
-			   << "Graphite cannot represent stmt:\n";
+			   << "Graphite cannot code-gen stmt:\n";
 			   print_gimple_stmt (dump_file, stmt, 0,
 					      TDF_VOPS | TDF_MEMSYMS));
 	      return false;